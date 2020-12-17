94caf5be3a02aa3181505760e61e97525c802ab0
Tran Thanh Long <longtranthanh6@gmail.com>
2020-12-17 17:27:03 +0700

    Add PhotoActivity

---
+++ app/src/main/java/vn/edu/usth/dropboxclient/PhotoActivity.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.dropboxclient;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
