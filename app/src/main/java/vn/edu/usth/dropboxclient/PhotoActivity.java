5e6cdadd6daffd51630c1d3ea7490fe591a15eed
Tran Thanh Long <longtranthanh6@gmail.com>
2020-12-18 23:20:35 +0700

    Fix PhotoActivity

--- app/src/main/java/vn/edu/usth/dropboxclient/PhotoActivity.java
+++ app/src/main/java/vn/edu/usth/dropboxclient/PhotoActivity.java
@@ -1,20 +1,14 @@
 package vn.edu.usth.dropboxclient;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PhotoActivity extends AppCompatActivity {
 
