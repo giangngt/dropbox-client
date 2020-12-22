b5854b9958f810de1ce4c4eedfa0eea5d0c11984
nolanspecter <59214460+nolanspecter@users.noreply.github.com>
2020-12-22 21:20:39 +0700

    Can view all sample images

--- app/src/main/java/vn/edu/usth/dropboxclient/PhotoActivity.java
+++ app/src/main/java/vn/edu/usth/dropboxclient/PhotoActivity.java
@@ -22,7 +22,7 @@ import android.widget.RelativeLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PhotoActivity extends AppCompatActivity {
-    private ImageView avatar1, avatar2;
+    private ImageView avatar1, avatar2,avatar3,avatar4,avatar5,avatar6,avatar7,avatar8,avatar9,avatar10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,8 +65,24 @@ public class PhotoActivity extends AppCompatActivity {
         });
         avatar1 = (ImageView)findViewById(R.id.imageView1);
         avatar2 = (ImageView)findViewById(R.id.imageView2);
+        avatar3 = (ImageView)findViewById(R.id.imageView3);
+        avatar4 = (ImageView)findViewById(R.id.imageView4);
+        avatar5 = (ImageView)findViewById(R.id.imageView5);
+        avatar6 = (ImageView)findViewById(R.id.imageView6);
+        avatar7 = (ImageView)findViewById(R.id.imageView7);
+        avatar8 = (ImageView)findViewById(R.id.imageView8);
+        avatar9 = (ImageView)findViewById(R.id.imageView9);
+        avatar10 = (ImageView)findViewById(R.id.imageView10);
         avatar1.setOnClickListener(mClick);
         avatar2.setOnClickListener(mClick);
+        avatar3.setOnClickListener(mClick);
+        avatar4.setOnClickListener(mClick);
+        avatar5.setOnClickListener(mClick);
+        avatar6.setOnClickListener(mClick);
+        avatar7.setOnClickListener(mClick);
+        avatar8.setOnClickListener(mClick);
+        avatar9.setOnClickListener(mClick);
+        avatar10.setOnClickListener(mClick);
     }
     private View.OnClickListener mClick = new View.OnClickListener() {
         @Override
@@ -89,9 +105,66 @@ public class PhotoActivity extends AppCompatActivity {
                 builder.addContentView(imageView, new RelativeLayout.LayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT,
                         ViewGroup.LayoutParams.WRAP_CONTENT));
-            } else {
+            }
+            else if(R.id.imageView2 == a) {
                 ImageView imageView = new ImageView(PhotoActivity.this);
                 imageView.setImageResource(R.drawable.avatar2);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else if(R.id.imageView3 == a) {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar3);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else if(R.id.imageView4 == a) {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar4);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else if(R.id.imageView5 == a) {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar5);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else if(R.id.imageView6 == a) {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar6);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else if(R.id.imageView7 == a) {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar7);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else if(R.id.imageView8 == a) {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar8);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else if(R.id.imageView9 == a) {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar9);
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            else {
+                ImageView imageView = new ImageView(PhotoActivity.this);
+                imageView.setImageResource(R.drawable.avatar10);
                 builder.addContentView(imageView, new RelativeLayout.LayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT,
                         ViewGroup.LayoutParams.WRAP_CONTENT));
