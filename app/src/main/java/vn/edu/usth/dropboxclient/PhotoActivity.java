b9e19fc76f866ec5d5fe2a9847936dc831fdcffe
nolanspecter <59214460+nolanspecter@users.noreply.github.com>
2020-12-22 20:38:44 +0700

    Temp comment zoom, trying new stuff with uri

--- app/src/main/java/vn/edu/usth/dropboxclient/PhotoActivity.java
+++ app/src/main/java/vn/edu/usth/dropboxclient/PhotoActivity.java
@@ -9,20 +9,29 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PhotoActivity extends AppCompatActivity {
+    private ImageButton avatar1, avatar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,125 +72,38 @@ public class PhotoActivity extends AppCompatActivity {
                 return false;
             }
         });
+        avatar1 = (ImageButton)findViewById(R.id.imageView1);
+        avatar2 = (ImageButton)findViewById(R.id.imageView2);
+        avatar1.setOnClickListener(mClick);
+        avatar2.setOnClickListener(mClick);
     }
-
-    public class ZoomActivity extends FragmentActivity {
-        private Animator currentAnimator;
-        private int shortAnimationDuration;
-
+    private View.OnClickListener mClick = new View.OnClickListener() {
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_zoom);
-
-            final View imgView = findViewById(R.id.imageView1);
-            imgView.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v) {
+            Dialog builder = new Dialog(PhotoActivity.this);
+            builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            builder.getWindow().setBackgroundDrawable(
+                    new ColorDrawable(android.graphics.Color.TRANSPARENT));
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
                 @Override
-                public void onClick(View view){
-                    zoomImageFromThumb(imgView,R.drawable.avatar1);
+                public void onDismiss(DialogInterface dialogInterface) {
+                    //nothing;
                 }
             });
-
-            shortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
-        }
-
-        private void zoomImageFromThumb(final View thumbView, int imageResId){
-            if (currentAnimator != null){
-                currentAnimator.cancel();
-            }
-
-            final ImageView expandedImageView = (ImageView) findViewById(R.id.expanded_image);
-            expandedImageView.setImageResource(imageResId);
-
-            final Rect startBound = new Rect();
-            final Rect endBound = new Rect();
-            final Point gloablOffset = new Point();
-
-            thumbView.getGlobalVisibleRect(startBound);
-            findViewById(R.id.container).getGlobalVisibleRect(endBound, gloablOffset);
-            startBound.offset(-gloablOffset.x, -gloablOffset.y);
-            endBound.offset(-gloablOffset.x, -gloablOffset.y);
-
-            float scale;
-            if((float) endBound.width() / endBound.height() > (float)startBound.width()/startBound.height()){
-                // Extend horizontally
-                scale = (float) startBound.height() / endBound.height();
-                float startWidth = scale * endBound.width();
-                float deltaWidth = (startWidth - startBound.width()) / 2;
-                startBound.left -= deltaWidth;
-                startBound.right += deltaWidth;
+            int a = v.getId();
+            Uri path;
+            if(R.id.imageView1 == a){
+                path = Uri.parse("android.resource://\"+getPackageName()+\"/drawable/avatar1");
             }
             else{
-                // Extend vertically
-                scale = (float) startBound.width() / endBound.width();
-                float startHeight = scale * endBound.height();
-                float deltaHeight = (startHeight - startBound.height()) / 2;
-                startBound.top -= deltaHeight;
-                startBound.bottom += deltaHeight;
+                path = Uri.parse("android.resource://\"+getPackageName()+\"/drawable/avatar2");
             }
-
-            thumbView.setAlpha(0f);
-            expandedImageView.setVisibility(View.VISIBLE);
-            expandedImageView.setPivotX(0f);
-            expandedImageView.setPivotX(0f);
-
-
-            AnimatorSet set = new AnimatorSet();
-            set
-                    .play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBound.left, endBound.left))
-                    .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBound.top, endBound.top))
-                    .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, scale, 1f))
-                    .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, scale, 1f));
-            set.setDuration(shortAnimationDuration);
-            set.setInterpolator(new DecelerateInterpolator());
-            set.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation){
-                    currentAnimator = null;
-                }
-
-                public void onAnimationCancel(Animator animation){
-                    currentAnimator = null;
-                }
-            });
-            set.start();
-            currentAnimator = set;
-
-            final float scaleFinal = scale;
-            expandedImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (currentAnimator != null){
-                        currentAnimator.cancel();
-                    }
-
-                    AnimatorSet set = new AnimatorSet();
-                    set.play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBound.left))
-                            .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBound.top))
-                            .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, scaleFinal))
-                            .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, scaleFinal));
-                    set.setDuration(shortAnimationDuration);
-                    set.setInterpolator(new DecelerateInterpolator());
-                    set.addListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation){
-                            thumbView.setAlpha(1f);
-                            expandedImageView.setVisibility(View.GONE);
-                            currentAnimator = null;
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animation){
-                            thumbView.setAlpha(1f);
-                            expandedImageView.setVisibility(View.GONE);
-                            currentAnimator = null;
-                        }
-                    });
-
-                    set.start();
-                    currentAnimator = set;
-                }
-            });
+            ImageView imageView = new ImageView(PhotoActivity.this);
+            imageView.setImageURI(path);
+            builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+            builder.show();
         }
-    }
+    };
 }
