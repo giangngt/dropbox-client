0f5423f3854227db530bcb178eec891c71888146
nolanspecter <59214460+nolanspecter@users.noreply.github.com>
2020-12-22 17:40:03 +0700

    Add Zoom. Still havent work

---
+++ app/src/main/java/vn/edu/usth/dropboxclient/ZoomActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.dropboxclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoom);
+    }
+}
