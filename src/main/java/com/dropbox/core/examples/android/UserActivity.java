07e230653a5a2dbfd4bb3d7c7408972e5518e8f0
Giang <truonggiang2704@gmail.com>
2021-01-24 18:46:03 +0700

    add fully functional example

---
+++ src/main/java/com/dropbox/core/examples/android/UserActivity.java
@@ -0,0 +1,96 @@
+package com.dropbox.core.examples.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.dropbox.core.examples.android.internal.OpenWithActivity;
+import com.dropbox.core.v2.users.FullAccount;
+
+import java.util.Arrays;
+
+
+/**
+ * Activity that shows information about the currently logged in user
+ */
+public class UserActivity extends DropboxActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_user);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        Button loginButton = (Button)findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DropboxActivity.startOAuth2Authentication(UserActivity.this, getString(R.string.app_key), Arrays.asList("account_info.read", "files.content.write", "files.content.read"));
+            }
+        });
+
+        Button filesButton = (Button)findViewById(R.id.files_button);
+        filesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(FilesActivity.getIntent(UserActivity.this, ""));
+            }
+        });
+
+        Button openWithButton = (Button)findViewById(R.id.open_with);
+        openWithButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openWithIntent = new Intent(UserActivity.this, OpenWithActivity.class);
+                startActivity(openWithIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (hasToken()) {
+            findViewById(R.id.login_button).setVisibility(View.GONE);
+            findViewById(R.id.email_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.name_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.type_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.files_button).setEnabled(true);
+            findViewById(R.id.open_with).setEnabled(true);
+        } else {
+            findViewById(R.id.login_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.email_text).setVisibility(View.GONE);
+            findViewById(R.id.name_text).setVisibility(View.GONE);
+            findViewById(R.id.type_text).setVisibility(View.GONE);
+            findViewById(R.id.files_button).setEnabled(false);
+            findViewById(R.id.open_with).setEnabled(false);
+        }
+    }
+
+    @Override
+    protected void loadData() {
+        new GetCurrentAccountTask(DropboxClientFactory.getClient(), new GetCurrentAccountTask.Callback() {
+            @Override
+            public void onComplete(FullAccount result) {
+                ((TextView) findViewById(R.id.email_text)).setText(result.getEmail());
+                ((TextView) findViewById(R.id.name_text)).setText(result.getName().getDisplayName());
+                ((TextView) findViewById(R.id.type_text)).setText(result.getAccountType().name());
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e(getClass().getName(), "Failed to get account details.", e);
+            }
+        }).execute();
+    }
+
+}
